2a86cf55ea5ff63e482265011f51076598284bb5
honebone <ryou4402@icloud.com>
2024-04-17 01:07:12 +0900

    リザルト画面に着手

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    void Awake()
+    {
+        CheckInstance();
+    }
+
+    void CheckInstance()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    /// <summary>’Tõ‰æ–Ê‚©‚ç’TõŒ‹‰Ê‰æ–Ê‚É‚¿‰z‚·“à—e</summary>
+    public class ExpeditionToResult
+    {
+
+    }
+}
