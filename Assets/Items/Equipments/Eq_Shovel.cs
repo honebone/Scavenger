788871e737b1c73164a3b2d0b6ba0db5c1bdc783
honebone <ryou4402@icloud.com>
2024-06-25 03:02:32 +0900

    装備品のSprite

--- Assets/Items/Equipments/Eq_Shovel.cs
+++ Assets/Items/Equipments/Eq_Shovel.cs
@@ -2,24 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Eq_SeawaterBucket : PA_Equipment
+public class Eq_Shovel : PA_Equipment
 {
-    [SerializeField] Action.ActionStatus attack;
+    [SerializeField] Action.ActionStatus actionStatus;
     [SerializeField] CharactersManager.SearchCharaCondition condition;
-    
+
     public override void OnTurnEnd()
     {
         List<Character> target = charactersManager.SearchCharaWithCondition(condition);
         if (target.Count > 0)
         {
-            Enqueue(attack, true, target);
+            Enqueue(actionStatus, true, new List<Character> { target.Choice() });
         }
     }
 
     public override string GetPAInfo_Base()
     {
         string s = equipmentStatus.GetInfo();
-        s += attack.GetInfo(false, new Character.CharacterStatus());
+        s += actionStatus.GetInfo(false, new Character.CharacterStatus());
         return s;
     }
 }
