8562a485f06a0749dc0238c8cd5932c277abe7de
honebone <ryou4402@icloud.com>
2025-02-05 17:17:12 +0900

    指揮官に着手せよ

---
+++ Assets/Character/Player/Commander/P_Comannder.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P_Comannder : PA_Personality
+{
+    public override void OnSomeoneFocus(List<Action.OnFocusParams> focusParamsList)
+    {
+        base.OnSomeoneFocus(focusParamsList);
+    }
+}
