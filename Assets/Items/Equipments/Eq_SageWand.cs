9bb14873d26af35070935563cbeba371ee8549e7
honebone <ryou4402@icloud.com>
2025-07-16 22:32:51 +0900

    狂気の仕様検討中

---
+++ Assets/Items/Equipments/Eq_SageWand.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eq_SageWand : PA_Equipment
+{
+    public override void OnRoundStart()
+    {
+        base.OnRoundStart();
+    }
+}
